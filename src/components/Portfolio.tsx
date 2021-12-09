0cb10656890770bdf479be9c3dd003277f0a2be6
Kurt <pandabat1@gmail.com>
2021-12-09 11:36:30 +0800

    [add] project info

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -10,18 +10,29 @@ const Portfolio:React.FC = () => {
                     <ul className="content">
                         <li>熟悉前端網頁技術(Html5+CSS3/JS/JQuery)</li>
                         <li>熟悉響應式網頁設計 ( RWD )</li>
+                        <li>擁有Material-UI及Chakra-UI框架開發經驗</li>
                         <li>理解 AJAX 並能夠介接 RESTful API</li>
-                        <li>熟悉 React 前端框架、React Hook，並會運用組件式開發與相關系統觀念</li>
-                        <li>熟悉Git工具使用</li>
+                        <li>熟悉 React 前端框架、React HookRedux狀態管理，並會運用組件式開發系統</li>
+                        <li>熟悉Git工具使用及Git指令</li>
                         <li>熟悉Drupal頁面呈現操作</li>
                     </ul>
                 </div>
                 <div>
                     <h4 className="title">後端技能</h4>
                     <ul className="content">
+                        <li>熟悉 Node.js，並會使用 FireBase 來做為後端的資料庫開發</li>
                         <li>Java簡易開發及Debug處理</li>
                         <li>熟悉SQL Server操作及Store Procedure開發</li>
                         <li>略懂Linux/Unix操作</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 className="title">其他</h4>
+                    <ul className="content">
+                        <li>擁有Microsoft Azure證照 - Microsoft Azure Developer Associate</li>
+                        <li>職業安全證照</li>
+                        <li>急救人員證照</li>
+                        <li>專案管理經驗</li>
                     </ul>
                 </div>
             </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import pomodoro from "../img/Pomodoro.jpg";
 import youtube from "../img/youtube.jpg";
+import bingo from "../img/Bingo.jpg";
+import cart from "../img/cart.jpg";
 
 const Project:React.FC = () => {
 
     return (
         <div className="project">
             <div className="projectDetail">
-                <img src={youtube} />
+                <img src={youtube} alt="youtube" />
                 <div className="info">
                     <div className="title">仿YouTube</div>
-                    <div className="detail">使用React+TypeScript串接 Youtube 的API 可實際查詢 與播放影片</div>
+                    <div className="detail">使用React+TypeScript串接 Youtube 的API 可實際查詢 與播放影片。</div>
                 </div>
                 <div className="button">
                     <div><a href="https://kurtzhou1.github.io/mySelf/">DEMO</a></div>
@@ -18,15 +20,28 @@ const Project:React.FC = () => {
                 </div>
             </div>
             <div className="projectDetail">
-                <img src={pomodoro} />
+                <img src={pomodoro} alt="pomodoro" />
                 <div className="info">
                     <div className="title">番茄鐘</div>
-                    <div className="detail">使用React+TypeScript搭配Redux及Hook的番茄鐘專案</div>
+                    <div className="detail">使用React+TypeScript搭配Redux及Hook的番茄鐘專案。</div>
                 </div>
-                <div className="detail"></div>
                 <div className="button">
                     <div><a href="https://kurtzhou1.github.io/Pomodoro_deploy/">DEMO</a></div>
                     <div><a href="https://github.com/kurtzhou1/Pomodoro">Github</a></div>
+                </div>
+            </div>
+            <div className="projectDetail special">
+                <img src={bingo} alt="bingo" />
+                <div className="info">
+                    <div className="title">Bingo遊戲專案(公司尾牙使用)</div>
+                    <div className="detail">參與前端使用React搭配Chakra UI切版，銜接FireBase偵測資料異動時即時傳送最新資訊至大螢幕端。</div>
+                </div>
+            </div>
+            <div className="projectDetail special">
+                <img src={cart} alt="bingo" />
+                <div className="info">
+                    <div className="title">家教平台專案</div>
+                    <div className="detail">參與計算購物車功能；並串接API，包含購物車商品、可使用折價券、提交訂單並至藍新金流作驗證</div>
                 </div>
             </div>
         </div>
